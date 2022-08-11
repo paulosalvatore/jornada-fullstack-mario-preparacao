2525f0d3efeec1f69a4451166411ec7aef623bd2
Paulo Salvatore <salvatoregames@gmail.com>
2022-08-11 08:59:49 -0300

    feat(frontend): add high score (WIP)

--- frontend/src/components/HighScore/HighScore.jsx
+++ frontend/src/components/HighScore/HighScore.jsx
@@ -1,5 +1,42 @@
 import "./HighScore.css";
 
 export default function HighScore() {
-  return <div className="high-score">HighScore</div>;
+  const scores = [
+    { name: "Paulo", score: 90 },
+    { name: "Ana", score: 30 },
+    { name: "Julio", score: 20 },
+  ];
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    alert("Score sent successfully!");
+  }
+
+  return (
+    <div className="high-score">
+      <div className="title">Congratulations!</div>
+
+      <div className="result">You made 90 points.</div>
+
+      <div className="title">High Scores</div>
+
+      <div className="body">
+        {scores.map((item, index) => (
+          <div key={`score_${index}`} className="high-score-item">
+            {item.name} - {item.score}
+          </div>
+        ))}
+      </div>
+
+      <div className="new-score">
+        <div className="title">Submit your score!</div>
+
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="Your name..." />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 }
